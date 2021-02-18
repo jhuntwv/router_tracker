6df15cd67922755f62b35d0e76e9a22b0db63260
Joshua Hunt <jhunt@westvirginia.com>
2021-02-18 16:46:24 -0500

    converting json manipulator to include xml

--- json_editor.py
+++ json_editor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import json
+import xmltodict
 
 def load_json(file):
     with open(file) as f:
@@ -10,5 +11,12 @@ def store_json(file, payload):
     with open(file, 'w') as f:
         json.dump(payload, f, indent = 4)
 
+def load_xml(file):
+    with open(file) as f:
+        return xmltodict.parse(f.read())
+
+def store_xml(file, payload):
+    with open(file, 'w') as f:
+        f.write(xmltodict.unparse(payload, pretty=True))
 
 
